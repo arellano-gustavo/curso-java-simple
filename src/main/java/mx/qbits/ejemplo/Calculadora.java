9bff281fb6d2b4dd922b45dec9fe3fc0ce5a5bdf
gerasmurx <gerasmurx@gmail.com>
2023-02-02 12:45:43 -0600

    esto da error por que no tengo permiso

--- src/main/java/mx/qbits/ejemplo/Calculadora.java
+++ src/main/java/mx/qbits/ejemplo/Calculadora.java
@@ -4,8 +4,5 @@ public class Calculadora {
   public int suma(int a, int b) {
     return a+b;
   }
-  public static void main(String...argv) {
-    Calculadora c = new Calculadora();
-    System.out.println("Resultado: "+ c.suma(2,7));
-  }
+  
 }
---
+++ src/main/java/mx/qbits/ejemplo/Principal.java
@@ -0,0 +1,10 @@
+package mx.qbits.ejemplo;
+
+public class Principal {
+
+	public static void main(String...argv) {
+	    Calculadora c = new Calculadora();
+	    System.out.println("Resultado: "+ c.suma(2,7));
+	  }
+
+}
