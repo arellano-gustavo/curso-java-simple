ac98603d5368301c72db65e2db2c456b7eac74fa
Gustavo Adolfo Arellano Sandoval <100727464+goose-bitso@users.noreply.github.com>
2023-02-02 12:11:41 -0600

    Update Calculadora.java

--- src/main/java/mx/qbits/ejemplo/Calculadora.java
+++ src/main/java/mx/qbits/ejemplo/Calculadora.java
@@ -4,4 +4,8 @@ public class Calculadora {
   public int suma(int a, int b) {
     return a+b;
   }
+  public static void main(String...argv) {
+    Calculadora c = new Calculadora();
+    Systerm.out.println("Resultado: "+ c.suma(2,7));
+  }
 }
