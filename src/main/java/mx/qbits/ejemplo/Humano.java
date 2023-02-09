3c3ca1e40f943cb7803713d89ac20a9d4d56d6ad
arellano-gustavo <arellano.gustavo@gmail.com>
2023-02-09 12:25:28 -0600

    agregando una interface

---
+++ src/main/java/mx/qbits/ejemplo/Humano.java
@@ -0,0 +1,8 @@
+package mx.qbits.ejemplo;
+
+public interface Humano {
+    public int calculaPrimero();
+    public float calculaSegundo(int a);
+    public double calculaTercer(String s);
+    public String calculaCuarto(int a, int b);
+}
